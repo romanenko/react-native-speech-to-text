aa8d47e74c00d8ef493f5be0da488c4c95a11d98
Michael Romanenko <michael@romanenko.kg>
2018-04-19 23:00:38 +0600

    Remove deprecated @Override statements

--- android/src/main/java/com/speechtotext/SpeechToTextPackage.java
+++ android/src/main/java/com/speechtotext/SpeechToTextPackage.java
@@ -9,17 +9,14 @@ import com.facebook.react.uimanager.ViewManager;
 
 public class SpeechToTextPackage implements ReactPackage {
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
-    @Override
     public List<NativeModule> createNativeModules(
             ReactApplicationContext reactContext) {
 
